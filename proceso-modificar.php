827d460776bcdcfcd61d574185cd9d4751669361
jimmy <jimmy22piedrahita@gmail.com>
2025-07-21 21:38:37 -0500

    Refactor image modification logic to include category ID retrieval and update queries

--- proceso-modificar.php
+++ proceso-modificar.php
@@ -2,14 +2,21 @@
 include("conexion.php");
 $id = $_REQUEST['id'];
 $categoria = $_POST['categoria'];
+
+// Obtener el ID de la categoría basado en el nombre
+$query_categoria = "SELECT id_categoria FROM categorias WHERE nombre = '$categoria'";
+$resultado_categoria = $conexion->query($query_categoria);
+$row_categoria = $resultado_categoria->fetch_assoc();
+$id_categoria = $row_categoria['id_categoria'];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fileError = $_FILES['imagen']['error'];
 
     if ($fileError === 4) {
-      $query = "update imagenes set categoria='$categoria' where id_imagen = '$id'";
+      $query = "UPDATE imagenes SET id_categoria='$id_categoria' WHERE id_imagen = '$id'";
     } else {
       $imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
-      $query = "update imagenes set categoria='$categoria', imagen='$imagen' where id_imagen = '$id'";
+      $query = "UPDATE imagenes SET id_categoria='$id_categoria', imagen='$imagen' WHERE id_imagen = '$id'";
     }
 }
  $resultado = $conexion->query($query);
